9de854eda4015e622e2cccbf9565eb5729c9946b
farabimahmud <farabi@tamu.edu>
2021-11-22 17:06:31 -0600

    misc: added script options to run pathfinder
    pathfinder script is runpathfinder.
    
    Change-Id: I14e15c969839ccc42fa9738fa9dc45545be0b7da

---
+++ runpathfinder.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+build/X86_MESI_Two_Level/gem5.opt \
+    --debug-flags=ReadingPCFile \
+    --debug-file=debug.out \
+    configs/example/se.py \
+    --num-cpus=64 \
+    --num-dirs=64 \
+    --network=garnet \
+    --topology=Mesh_XY \
+    --mesh-row=8 \
+    --ruby \
+    --caches \
+    --l2cache \
+    --num-l2caches=64 \
+    --fast-forward=9223372036854775807 \
+    --cpu-type=DerivO3CPU \
+    --cmd="pathfinder" \
+    --read_pc_list_from_file \
+    --pc_list_filename="/home/farabi/smart-noc/pcs/pathfinder.txt" \
+    --options="100 1 64"  > log_file 2>&1 
+
+
+#     --enable-smart \
+#    --smart_hpcmax=6 \
+#    --smart_dest_bypass \
+
+   
